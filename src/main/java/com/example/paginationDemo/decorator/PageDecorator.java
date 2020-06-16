8332d2c948feb00eb0ef115bf2635e2155d42c65
cramallo <cramallo65@gmail.com>
2020-06-16 17:22:24 -0300

    add link attributes

--- src/main/java/com/example/paginationDemo/decorator/PageDecorator.java
+++ src/main/java/com/example/paginationDemo/decorator/PageDecorator.java
@@ -1,35 +1,43 @@
 package com.example.paginationDemo.decorator;
 
 import org.springframework.data.domain.Page;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.Links;
 
 import java.util.List;
 
 public class PageDecorator {
     private final Page page;
+    private final Links pageLinks;
+    private static String PREV_RELATION = "prev";
+    private static String NEXT_RELATION = "next";
+    private static String FIRST_RELATION = "first";
+    private static String LAST_RELATION = "last";
+    private static String SELF_RELATION = "self";
 
-
-    public List getContent() {
-        return this.page.getContent();
-    }
-
-    public PageDecorator(Page page) {
+    public PageDecorator(final Page page, final Links pageLinks) {
         this.page = page;
+        this.pageLinks = pageLinks;
     }
 
-    public int getTotalPages() {
-        return page.getTotalPages();
+    public List getData() {
+        return this.page.getContent();
     }
 
     public long getTotalElements() {
         return page.getTotalElements();
     }
 
-    public int getCurrentPage() {
-        return page.getNumber() + 1;
-    }
-
     public int getLimit() {
         return page.getSize();
+    }
+
+    public long getOffset() {
+        return page.getPageable().getOffset();
+    }
+
+    public int getTotalPages() {
+        return page.getTotalPages();
     }
 
     public boolean isLast() {
@@ -40,6 +48,18 @@ public class PageDecorator {
         return page.getNumberOfElements();
     }
 
+    public int getPreviousPage() {
+        return page.previousOrFirstPageable().getPageNumber() + 1;
+    }
+
+    public int getCurrentPage() {
+        return page.getNumber() + 1;
+    }
+
+    public int getNextPage() {
+        return page.nextOrLastPageable().getPageNumber() + 1;
+    }
+
     public boolean isFirst() {
         return page.isFirst();
     }
@@ -48,14 +68,28 @@ public class PageDecorator {
         return page.isEmpty();
     }
 
-    public int getPreviousPage() {
-        return page.previousOrFirstPageable().getPageNumber() + 1;
+    public String getPreviousPageLink() {
+        return !page.isEmpty() ? this.getLink(PREV_RELATION) : this.getLink(LAST_RELATION);
     }
 
-    public int nextPage() {
-        return page.nextOrLastPageable().getPageNumber() + 1;
+    public String getNextPageLink() {
+        return this.getLink(NEXT_RELATION);
     }
 
-    //TODO: ADD LINKS AND OFFSET
+    public String getFirstPageLink() {
+        return this.getLink(FIRST_RELATION);
+    }
+
+    public String getLastPageLink() {
+        return this.getLink(LAST_RELATION);
+    }
+
+    public String getCurrentPageLink() {
+        return !page.isEmpty() ?  this.getLink(SELF_RELATION) : this.getLink(LAST_RELATION);
+    }
+
+    private String getLink(final String relation) {
+        return this.pageLinks.getLink(relation).map(Link::getHref).orElse(null);
+    }
 
 }
